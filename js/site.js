e69f70f133c9308078615e5574213da7f8d2fcfc
amitavag <amitava.ghosh@pearson.com>
2015-09-19 19:08:00 -0500

    no message

--- js/site.js
+++ js/site.js
@@ -38,36 +38,14 @@ function enableAd () {
 	$('#adSection').show();
 }
 
-function readXML() {
-	try {
-        var xmlPath = "template/data.xml";
-        $.ajax({
-            type: "GET",
-            url: xmlPath,
-            dataType: "xml",
-            success: successXML
-        });
-    } catch (e) {
-        alert("Error while reading XML; Description – " + e.description);
-    }
-}
-
 var criteriaData = '';
+var globalTemplate = '<tr class=""><td><div style="clear:both;width:100%"><div class="leftitem" style="padding-right: 20px;float:left;width: 30%"><img title="##TITLE##" alt="##TITLE##" src="##PIC##"></div><div style="float:left;width:60%"><div class="title"><div style="float:left;" class="##TYPE##">&nbsp;</div><div style="float:left;width:90%"><a alt="#TITLE##" title="##TITLE##" class="noStyle" href="##URL##.html">##TITLE##</a></div></div><div class="desc">##DESC##</div></div></div></td></tr><tr class="blankTR"></tr>';
 
-function successXML(xmlData) {
-	var cri = criteriaData.split(" ");
-	var $element = $(xmlData).find('title').filter(function() {
-		var data = ($(this).text()).toLowerCase();
-		for (var i=0; i<cri.length; i++) {
-			if (data.indexOf(cri[0]) > -1) {
-				return true;
-			}
-		}
-	}).closest('element');	
+function populateData(dataElement) {
 	var htmlForm = '<div class="recipePage"><table class="dataTable">';
-	if ($element.length > 0) {
-		for (var i=0; i<$element.length; i++) {
-			htmlForm += populateSearchResult($element[i]);
+	if (dataElement.length > 0) {
+		for (var i=0; i<dataElement.length; i++) {
+			htmlForm += populateSearchResult(dataElement[i]);
 		}
 		htmlForm += '</table></div>';
 		$('.middleData').html(htmlForm);
@@ -75,50 +53,6 @@ function successXML(xmlData) {
 		$('#searchMdl').html('');
 		$('#searchMdl').addClass('f30px');
 		$('#searchMdl').html('No results found, please try again with different criteria.');
-	}
-}
-
-function populateSearchResult(elementData) {
-	var url = $('url', elementData).text();
-	var title = $('title', elementData).text();
-	var thumb = $('thumb', elementData).text();
-	var desc = $('shortDesc', elementData).text();
-	var type = $('type', elementData).text();
-	var template = '<tr class=""><td><div style="clear:both;width:100%"><div class="leftitem" style="padding-right: 20px;float:left;width: 30%"><img title="##TITLE##" alt="##TITLE##" src="##PIC##"></div><div style="float:left;width:60%"><div class="title"><div style="float:left;" class="##TYPE##">&nbsp;</div><div style="float:left;width:90%"><a alt="#TITLE##" title="##TITLE##" class="noStyle" href="##URL##.html">##TITLE##</a></div></div><div class="desc">##DESC##</div></div></div></td></tr><tr class="blankTR"></tr>';
-	template = replaceAll('##TITLE##', title, template);
-	template = replaceAll('##DESC##', desc, template);
-	template = replaceAll('##TYPE##', type, template);
-	template = replaceAll('##PIC##', thumb, template);
-	template = replaceAll('##URL##', url, template);
-	return template;
-}
-
-function replaceAll(find, rep, str) {
-	var re = new RegExp(find, 'g');
-	str = str.replace(re, rep);
-	return str;
-}
-
-function enterPressCheck(B){
-	var A;
-	if(window.event){
-		A=window.event.keyCode;
-	}else{
-		A=B.which;
-	}
-	if(A==13){
-		searchData();
-	}else{
-		return"false";
-	}
-}
-
-function searchData(){
-	var data = $('#searchBox').val().trim();
-	if (data != '') {
-		location.href = 'search.html?criteria=' + data;
-	} else {
-		$('#searchBox').placeholder = 'Search recipes ...';
 	}
 }
 
@@ -133,16 +67,4 @@ function searchForRecipe() {
 		$('#searchMdl').addClass('f30px');
 		$('#searchMdl').html('Incorrect search critera, please try again.');
 	}
-}
-
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      return pair[1];
-    }
-  } 
-  return "";
 }
---
+++ js/siteCommon.js
@@ -0,0 +1,89 @@
+function enterPressCheck(B){
+    var A;
+    if(window.event){
+        A=window.event.keyCode;
+    }else{
+        A=B.which;
+    }
+    if(A==13){
+        searchData();
+    }else{
+        return"false";
+    }
+}
+
+function readXML() {
+	try {
+        var xmlPath = "template/data.xml";
+        $.ajax({
+            type: "GET",
+            url: xmlPath,
+            dataType: "xml",
+            success: successXML
+        });
+    } catch (e) {
+        alert("Error Loading Recipe from Server: " + e.description);
+    }
+}
+
+function populateSearchResult(elementData) {
+    var url = $('url', elementData).text();
+    var title = $('title', elementData).text();
+    var thumb = $('thumb', elementData).text();
+    var desc = $('shortDesc', elementData).text();
+    var type = $('type', elementData).text();
+    var typeDesc = '';
+    if (type == 'vegItem') {
+        typeDesc = 'Veg Item';
+    } else {
+        typeDesc = 'Non-Veg Item';
+    }
+    var template = globalTemplate;
+    template = replaceAll('##TITLE##', title, template);
+    template = replaceAll('##DESC##', desc, template);
+    template = replaceAll('##TYPE##', type, template);
+    template = replaceAll('##PIC##', thumb, template);
+    template = replaceAll('##URL##', url, template);
+    template = replaceAll('##TYPE_DESC##', typeDesc, template);
+    return template;
+}
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  } 
+  return "";
+}
+
+function replaceAll(find, rep, str) {
+    var re = new RegExp(find, 'g');
+    str = str.replace(re, rep);
+    return str;
+}
+
+function successXML(xmlData) {
+    var cri = criteriaData.split(" ");
+    var $element = $(xmlData).find('title').filter(function() {
+        var data = ($(this).text()).toLowerCase();
+        for (var i=0; i<cri.length; i++) {
+            if (data.indexOf(cri[0]) > -1) {
+                return true;
+            }
+        }
+    }).closest('element');  
+    populateData($element);
+}
+
+function searchData(){
+    var data = $('#searchBox').val().trim();
+    if (data != '') {
+        location.href = 'search.html?criteria=' + data;
+    } else {
+        $('#searchBox').placeholder = 'Search recipes ...';
+    }
+}
