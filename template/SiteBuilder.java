1a094d82d6684e03726b65df3a0a9a8f401e6a7a
amitavag <amitava.ghosh@pearson.com>
2015-09-21 23:03:49 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -268,7 +268,7 @@ public class SiteBuilder {
 		}
 		selfCopy(templatePath + "template/SiteBuilder.java", processor);
 		System.out.println("Processed ...");
-		getAllImages(basePath + "recipeimages");
+		//getAllImages(basePath + "recipeimages");
 	}
 	
 	private static void generateTagHTML(String tag, String templatePath, NodeList nList, String baseTemplatePath, int count, String tagDataStr) {
