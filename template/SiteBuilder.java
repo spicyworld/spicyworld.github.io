c450e8747679b2edd7b782a2909fb643706c4c51
amitavag <amitava.ghosh@pearson.com>
2015-09-27 01:10:18 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -187,7 +187,7 @@ public class SiteBuilder {
 				data = data.replace(" ", "-");
 				htmlTags += "<span data-weight=\"" + entry.getValue() + "\"><a href=\"" + data + "-tag.html\">" + entry.getKey() + "</a></span>";
 				count++;
-				String h1Tag = "<h1 class='headerFont'>Recipes related to <i>" + entry.getKey() + "</i></h1>";
+				String h1Tag = "<h1 class='headerFont'>Recipes tagged as <i>'" + entry.getKey() + "'</i></h1>";
 				generateTagHTML(data, tag_data_template, nList, templatePath, count, entry.getKey(), h1Tag);
 				siteMapData += "<url><loc>http://spicyworld.in/" + data + "-tag.html</loc></url>";
 			}
