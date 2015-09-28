ca28a2a3c983e88b6c3f6748250b42e601e80135
amitavag <amitava.ghosh@pearson.com>
2015-09-28 18:40:51 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -29,7 +29,7 @@ import org.w3c.tidy.Tidy;
 
 public class SiteBuilder {
 	
-	public static String buildNo = "?sessionId=122";
+	public static String buildNo = "?sessionId=123";
 
 	public static void main(String[] args) {
 		String img = "butter-chicken";
