860b65a2c2d8e87c859613fa158a1a8125ea2d2b
amitavag <amitava.ghosh@pearson.com>
2015-10-03 22:40:54 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -280,6 +280,24 @@ public class SiteBuilder {
 			fileData = fileData.replaceAll("##ONLOAD_CALL##", "enableAd();searchForRecipe();");
 			saveFile(basePath + "search.html", fileData);
 			
+			
+			//About Me Page
+			fileData = readFile(basePath + "template/template.html");
+			fileData = fileData.replace("##TITLE_DATA##", "About Me | Spicy World by Arpita");
+			fileData = fileData.replace("##MIDDLE_DATA##", "<div><img src='images/about.jpg' width='100%'/>"
+					+ "<div id='aboutData'>"
+					+ "Hello Friends, <br/><br/>Arpita is a daughter and homemaker from two lovely Bengali families. At present she lives in Austin, Texas with her husband Amitava.<br/><br/>They both are originally from greater Kolkata and real food lovers.<br/><br/>Cooking, learning about new recipes, listening and singing old songs in lonely afternoons are her hobbies. Arpita is also a big fan and follower of authentic bengali cooking and very much all kinds of indian street foods. Everyday as a self taught cook she paints her food with spices, colors, love and care. Behind everything Amitava is her real inspiration. After marriage, getting compliments from husband about cooking is a great achievment.<br/><br/>So, she heartily invites you all to take a colorful journey through her little \"Spicy World\" ..."
+					+ "</div></div>");
+			fileData = fileData.replace("##about_sel##", "selected");
+			fileData = fileData.replaceAll("##BUILD_NO##", buildNo);
+			fileData = fileData.replaceAll("##ADDITIONAL_SCRIPTS##", "");
+			fileData = fileData.replaceAll("##KEYWORD_DATA##", "About me, About Spicy World");
+			fileData = fileData.replaceAll("##DESC_DATA##", "Welcome to Spicy World by Arpita Ghosh Das. Here are the cast and crew of Spicy World.");
+			fileData = fileData.replaceAll("##IMG_DATA##", "images/home/home3.jpg");
+			fileData = fileData.replaceAll("##URL_DATA##", "/about-me.html");
+			fileData = fileData.replaceAll("##ONLOAD_CALL##", "");
+			saveFile(basePath + "about-me.html", fileData);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
