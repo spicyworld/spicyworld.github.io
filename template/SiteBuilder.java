c71c31b32e06bc7b4f53df0263dd705cb40e9feb
amitavag <amitava.ghosh@pearson.com>
2015-10-14 17:28:09 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -531,7 +531,6 @@ public class SiteBuilder {
 			tags += "</div>";
 			out += tags;
 		} catch (Exception e) {} 
-		
 		out += "<div style=\"clear:both;padding-top:20px;padding-bottom:20px;\"><div id='comments' class='commentHeader'>Leave Your Comments</div>"
 		+ "<div class='disqus_thread_class'><div id=\"disqus_thread\"></div><script type=\"text/javascript\"> var disqus_shortname = 'spicyworld';  (function() {var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true; dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);  })();</script></div></div>";
 		
