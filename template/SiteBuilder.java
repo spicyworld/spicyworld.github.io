4739a47dcbd700dda931e0f8c74f601edff287be
amitavag <amitava.ghosh@pearson.com>
2015-10-14 17:34:28 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -470,13 +470,15 @@ public class SiteBuilder {
 				+ "<div style=\"float:left;\"><a href=\"//www.pinterest.com/pin/create/button/\" data-pin-do=\"buttonBookmark\"  data-pin-color=\"red\"><img src=\"//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_20.png\" /></a><script type=\"text/javascript\" async defer src=\"//assets.pinterest.com/js/pinit.js\"></script>"	 
 				+ "</div>"
 				// Pinterest Ends
-				+ "<div class='likePlace'><div class=\"rw-ui-container\"></div></div>"
 				// Twitter Starts
 				+ "<div style=\"float:left;height:10px;padding-left:10px;\">"
 				+ "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-url=\"http://spicyworld.in/" + url + ".html\" data-text=\"" + title + "\" data-via=\"amitava3g\"></a>"
 				+ "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
 				+ "</div>"
 				// Twitter Ends
+				// Rating Star Starts
+				+ "<div class='likePlace'><div class=\"rw-ui-container\"></div></div>"
+				// Rating Syar ends
 				// Email Starts
 				//+ "<div style=\"float:left;padding-left:10px;\"><a  title='Send/Share via Email' title='Send/Share via Email' "
 				//+ "href=\"mailto:?subject=" + title + " Recipe at Spicy World&body=" + desc + "\n Visit Spicy World (http://spicyworld.in/" + url + ".html?emailFlag=Y) for detailed recipe.\">"
