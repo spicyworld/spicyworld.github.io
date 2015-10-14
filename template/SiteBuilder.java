a096d76422ba2695bca9fbd0f93c5122fbd8f95b
amitavag <amitava.ghosh@pearson.com>
2015-10-14 17:01:35 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -34,14 +34,14 @@ public class SiteBuilder {
 	public static String aboutPageData = "Hello Friends, <br/><br/>Arpita is a daughter and homemaker from two lovely Bengali families. At present she lives in Austin, Texas with her husband Amitava.<br/><br/>They both are originally from greater Kolkata and real food lovers.<br/><br/>Cooking, learning about new recipes, listening and singing old songs in lonely afternoons are her hobbies. Arpita is also a big fan and follower of authentic bengali cooking and very much all kinds of indian street foods. Everyday as a self taught cook she paints her food with spices, colors, love and care. Behind everything Amitava is her real inspiration. After marriage, getting compliments from husband about cooking is a great achievment.<br/><br/>So, she heartily invites you all to take a colorful journey through her little \"Spicy World\" ...";
 	
 	public static void main(String[] args) {
-		String img = "fish-muro-muger-daal";
-		//createImage("/Volumes/Pearson/spicyworld/template/originals/" + img + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/" + img + ".jpg", 1000);
-		//createImage("/Volumes/Pearson/spicyworld/template/originals/" + img + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/thumb/" + img + ".jpg", 330);
-		/*for (int i=1;i<10; i++) {
-			img = "fish-muro-muger-daal-" + i;
-			createImage("/Volumes/Pearson/spicyworld/template/originals/" + img + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/" + img + ".jpg", 1000);
+		String img = "bhatura";
+		createImage("/Volumes/Pearson/spicyworld/template/originals/" + img + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/" + img + ".jpg", 1000);
+		createImage("/Volumes/Pearson/spicyworld/template/originals/" + img + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/thumb/" + img + ".jpg", 330);
+		for (int i=1;i<=13; i++) {
+			String limg = img + "-" + i;
+			createImage("/Volumes/Pearson/spicyworld/template/originals/" + limg + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/" + limg + ".jpg", 1000);
 		}
-		System.exit(1);*/
+		//System.exit(1);
 		String basePath = "/Volumes/Pearson/spicyworld/";
 		String templatePath = basePath;
 		String processor = "/Users/vghosam/Documents/workspace/test/src/SiteBuilder.java";
