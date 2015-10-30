8c1d2db5375a14f967a45928e654d5df2d9be39f
amitavag <amitava.ghosh@pearson.com>
2015-10-30 16:43:44 -0500

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -40,13 +40,13 @@ public class SiteBuilder {
 	public static String aboutPageData = "Hello Friends, <br/><br/>Arpita is a daughter and homemaker from two lovely Bengali families. At present she lives in Austin, Texas with her husband Amitava.<br/><br/>They both are originally from greater Kolkata and real food lovers.<br/><br/>Cooking, learning about new recipes, listening and singing old songs in lonely afternoons are her hobbies. Arpita is also a big fan and follower of authentic bengali cooking and very much all kinds of indian street foods. Everyday as a self taught cook she paints her food with spices, colors, love and care. Behind everything Amitava is her real inspiration. After marriage, getting compliments from husband about cooking is a great achievment.<br/><br/>So, she heartily invites you all to take a colorful journey through her little \"<a href='http://spicyworld.in'>Spicy World</a>\" ...";
 	
 	public static void main(String[] args) {
-		/*String img = "chicken-hakka-noodles";
+		String img = "dhaniya-chicken";
 		createImage("/Volumes/Pearson/spicyworld/template/recipeimages/" + img + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/" + img + ".jpg", 1000);
 		createImage("/Volumes/Pearson/spicyworld/template/recipeimages/" + img + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/thumb/" + img + ".jpg", 330);
 		for (int i=1;i<=12; i++) {
 			String limg = img + "-" + i;
 			createImage("/Volumes/Pearson/spicyworld/template/recipeimages/" + limg + ".jpg", "/Volumes/Pearson/spicyworld/recipeimages/" + limg + ".jpg", 1000);
-		}*/
+		}
 		//System.exit(1);
 		String basePath = "/Volumes/Pearson/spicyworld/";
 		String templatePath = basePath;
@@ -739,7 +739,7 @@ public class SiteBuilder {
 				BufferedImage bISmallImage = Scalr.resize(bufimage,
 						destinationWidth); // after this line my dimensions in
 											// bISmallImage are correct!
-				ImageIO.write(bISmallImage, "jpg", smallImage); // but my
+				ImageIO.write(bISmallImage, "jpeg", smallImage); // but my
 																// smallImage
 																// has the same
 																// dimension as
