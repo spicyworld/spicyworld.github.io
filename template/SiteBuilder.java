1063860fb37efaf1f1cc588b8f51694a71aef9dd
amitavag <amitava.ghosh@pearson.com>
2015-11-02 01:16:55 -0600

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -84,7 +84,7 @@ public class SiteBuilder {
 				+ "<a class='download' title='Spicy World : Cook Book'  alt='Spicy World : Cook Book' href='Spicy-World-Cook-Book.pdf' target='_blank' onclick=\"ga('send', 'event', 'PDF', 'download', this.href);\">Download CookBook</a>"
 				+ "&nbsp;&nbsp;To get the full CookBook, please follow our <a target='_blank' href='https://www.facebook.com/spicyworldrecipes' onclick=\"ga('send', 'event', 'Facebook', 'Website Cook Book', this.href);\">Facebook Page</a>.</div>";
 		String latest3DataForHomePage = "<div class=\"middleTop\"><div class=\"left\">"
-				+ "<div class=\"data\"><p>Easy and Simple Recipes make your cooking faster and your food delicious. Check out our recipes.</p>"
+				+ "<div class=\"data\"><p><h1>Spicy World's Easy and Simple Recipes make your cooking faster and your food delicious. Check out our recipes.</h1></p>"
 				+ "<a href=\"http://spicyworld.in/recipes.html\">Recipes</a></div></div>"
 				+ "<div class=\"middle\">&nbsp;</div><div class=\"right\">"
 				+ "<div id=\"slider1_container\" style=\"position: relative; top: 0px; left: 0px; height: 300px;\">"
