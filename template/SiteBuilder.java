3b787e97a2d4c68f6c50ad77ca2370ab08efd2c2
amitavag <amitava.ghosh@pearson.com>
2015-11-17 18:54:57 -0600

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -670,7 +670,7 @@ public class SiteBuilder {
 		}
 		out += "<div class='botNextPrev'>" + oldNew + "</div>" + relatedRecipes + "<div style=\"clear:both;padding-top:20px;padding-bottom:20px;\"><div id='comments' class='commentHeader'>Leave Your Comments</div>"
 		//+ "<div class='disqus_thread_class'><div id=\"disqus_thread\"></div><script type=\"text/javascript\"> var disqus_shortname = 'spicyworld';  (function() {var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true; dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);  })();</script></div>"
-		+ "<div class=\"fb-comments\" data-href=\"http://spicyworld.in/" + url + ".html\" data-numposts=\"5\"></div>" 
+		+ "<div data-width=\"100%\" class=\"fb-comments\" data-href=\"http://spicyworld.in/" + url + ".html\" data-numposts=\"5\"></div>" 
 		+ "</div>";
 		
 		
