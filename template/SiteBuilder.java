8d55b7088fe0c09d3c1205888f755399cc80fb12
amitavag <amitava.ghosh@pearson.com>
2015-12-20 15:51:45 -0600

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -58,13 +58,14 @@ public class SiteBuilder {
 	
 	public static void main(String[] args) {
 		String basePath = "/Volumes/Pearson/spicyworld/";
-		String img = "potato-stew";
+		
+		/*String img = "matar-kachori";
 		createImage(basePath + "/template/recipeimages/" + img + ".jpg", basePath + "/recipeimages/" + img + ".jpg", 1500, true, basePath);
 		createImage(basePath + "/template/recipeimages/" + img + ".jpg", basePath + "/recipeimages/thumb/" + img + ".jpg", 330, true, basePath);
-		for (int i=1;i<=7; i++) {
+		for (int i=1;i<=14; i++) {
 			String limg = img + "-" + i;
 			createImage(basePath + "/template/recipeimages/" + limg + ".jpg", basePath + "/recipeimages/" + limg + ".jpg", 1500, true, basePath);
-		}
+		}*/
 		//System.exit(1);
 		
 		
