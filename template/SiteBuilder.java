a4a4befd7da01f2942b7d53958268599cec2ddcb
amitavag <amitava.ghosh@pearson.com>
2016-01-03 14:48:23 -0600

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -737,20 +737,16 @@ public class SiteBuilder {
 			out += tags;
 		} catch (Exception e) {} 
 		if (relatedRecipes!=null && relatedRecipes.length()>0) {
-			relatedRecipes = "<div id='relatedRecipes' class='relatedRecipes'>You may also like</div><div class='clear relatedRecipesData'>" + relatedRecipes + "</div>";
+			relatedRecipes = "<br/><div id='relatedRecipes' class='relatedRecipes'>You may also like</div><div class='clear relatedRecipesData'>" + relatedRecipes + "</div>";
 		}
 		out += "<div class='botNextPrev'>" + oldNew + "</div>" + relatedRecipes
-		+ "<br/>"
-		// Bottom Ad section
-		//+ "<div style='margin-left:auto;margin-right:auto;text-align:center !important;'><script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script><ins class=\"adsbygoogle\" style=\"display:inline-block;width:728px;height:90px\" data-ad-client=\"ca-pub-7901069235725761\" data-ad-slot=\"1981019539\"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>"
+		+ "<br/><br/>"
+				+"<div id='relatedRecipesTags' class='relatedRecipes'>Categories</div><div class='bottomLinksTags'>" + htmlTags.replace("href=", "onclick=\"ga('send', 'event', 'Tag Click', 'Tag Click: recipe_bottom', this.href);\" href=") + "</div>"
 		// For comment section
-		+ "<div style=\"clear:both;padding-top:20px;padding-bottom:20px;\"><div id='comments' class='commentHeader'>Leave Your Comments</div>"
+		+ "<br/><div style=\"clear:both;padding-top:20px;padding-bottom:20px;\"><div id='comments' class='commentHeader'>Leave Your Comments</div>"
 		// For Disqus
 		+ "<div class='disqus_thread_class'><div id=\"disqus_thread\"></div><script type=\"text/javascript\"> var disqus_shortname = 'spicyworld';  (function() {var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true; dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);  })();</script></div>"
-		// Facebook
-		//+ "<div data-width=\"100%\" class=\"fb-comments\" data-href=\"http://spicyworld.in/" + url + ".html\" data-numposts=\"5\"></div>" 
-		+ "</div>"
-				+"<div id='relatedRecipesTags' class='relatedRecipes'>Categories</div><div class='bottomLinksTags'>" + htmlTags.replace("href=", "onclick=\"ga('send', 'event', 'Tag Click', 'Tag Click: recipe_bottom', this.href);\" href=") + "</div>";
+		+ "</div>";
 		
 		
 		String fileData = readFile(templatePath + "template/template.html");
