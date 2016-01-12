d7e9f9d881264b896dbe2ddf5f0784caba96b558
amitavag <amitava.ghosh@pearson.com>
2016-01-11 23:29:46 -0600

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -21,6 +21,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -44,6 +46,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.imgscalr.Scalr;
 import org.imgscalr.Scalr.Method;
 import org.imgscalr.Scalr.Mode;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -56,7 +59,7 @@ import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 public class SiteBuilder {
 	
-	public static String buildNo = "?sessionId=224";
+	public static String buildNo = "?sessionId=225";
 	public static String imgBuildNo = "?sessionId=214";
 	public static String pinterestData = "<script async data-pin-color=\"red\" data-pin-hover=\"true\" defer src=\"//assets.pinterest.com/js/pinit.js\"></script>";
 	public static String aboutPageData = "Hello Friends, <br/><br/><h1 class='specialH1'>Arpita</h1>&nbsp;is a daughter and homemaker from two lovely Bengali families. At present she lives in Austin, Texas with her husband Amitava.<br/><br/>They both are originally from greater Kolkata and real food lovers.<br/><br/>Cooking, learning about new recipes, listening and singing old songs in lonely afternoons are her hobbies. Arpita is also a big fan and follower of authentic bengali cooking and very much all kinds of indian street foods. Everyday as a self taught cook she paints her food with spices, colors, love and care. Behind everything Amitava is her real inspiration. After marriage, getting compliments from husband about cooking is a great achievement.<br/><br/>So, she heartily invites you all to take a colorful journey through her little \"<a href='http://spicyworld.in'>Spicy World</a>\" ...<br/><br/>Contact us: <u><a href='mailto:contact@spicyworld.in' target='_top' onclick=\"ga('send', 'event', 'Email Click', 'Email Click: Btn Click', this.href);\">contact@spicyworld.in</a></u>";
@@ -662,10 +665,17 @@ public class SiteBuilder {
 		if (steps.contains("recipeimages")) {
 			steps = "<div class='steps-image'>" + steps.replace("<img ", "<img alt='Cooking Step: " + title + "' title='Cooking Step: " + title + "' ") + "</div>";
 		}
+		DateFormat fullDf = DateFormat.getDateInstance(DateFormat.MEDIUM);
+		String datepub;
+		try {
+			datepub = fullDf.format(new Date(eElement.getElementsByTagName("pubDate").item(0).getTextContent()));
+		} catch (Exception e1) {
+			datepub = eElement.getElementsByTagName("pubDate").item(0).getTextContent();
+		}
 		
 		out = "<div vocab='http://schema.org/' typeof='Recipe'><div class='h2Class'><div style=\"clear:both\"><h1 property='name' id='title'>"
 				+ title
-				+ "</h1></div><div style=\"clear:both\">"
+				+ "</h1></div><div class='copyright copy" + type + "'><i>&copy; Spicy World</i>, Published on: <i>" + datepub + "</i></div><div style=\"clear:both\">"
 				// Pinterest Starts
 				+ "<div style=\"float:left;\"><a href=\"//www.pinterest.com/pin/create/button/\" data-pin-do=\"buttonBookmark\"  data-pin-color=\"red\"><img src=\"//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_20.png\" /></a><script type=\"text/javascript\" async defer src=\"//assets.pinterest.com/js/pinit.js\"></script>"	 
 				+ "</div>"
