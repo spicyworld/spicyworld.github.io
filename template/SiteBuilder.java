c3dc5bdfe4449f8aa3d44eed8dc944fd5a8abc05
amitavag <amitava.ghosh@pearson.com>
2016-01-13 20:48:39 -0600

    no message

--- template/SiteBuilder.java
+++ template/SiteBuilder.java
@@ -71,12 +71,12 @@ public class SiteBuilder {
 		//Compress files
 		//compressFiles(basePath, processorBasePath);
 		
-		String img = "macher-chop";
+		/*String img = "macher-chop";
 		createImage(basePath + "/template/recipeimages/" + img + ".jpg", basePath + "/recipeimages/" + img + ".jpg", 1500, true, basePath);
 		for (int i=1;i<=13; i++) {
 			String limg = img + "-" + i;
 			createImage(basePath + "/template/recipeimages/" + limg + ".jpg", basePath + "/recipeimages/" + limg + ".jpg", 1500, true, basePath);
-		}
+		}*/
 		
 		//transformAllImages(basePath);
 		//System.exit(1);
