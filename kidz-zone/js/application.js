3f1ee8f2f2051b681767a30a1ea3b0cefba5a7d0
AMITAVA\amita <amitava3.g@gmail.com>
2020-02-03 20:31:52 -0600

    Recipe update

--- kidz-zone/js/application.js
+++ kidz-zone/js/application.js
@@ -1,12 +1,31 @@
+var counter = 0;
+var customData = [];
+		
 function loadPage(page) {
 	$('.nav-link').removeClass('active');
     $('#' + page).addClass('active');
     page = page.replace('-head', '');
 	console.log("Loading " + page + ".html");
 	$("#middle-data-container").load("pages/" + page + ".html");
+	$('.dim-areas').addClass('dim-areas');
 }
 
-var numbers = [
-		{"key": "1", "val": "ONE"}, 
-		{"key": "2", "val": "TWO"}
-];
+function changeCounter(type) {
+	if (type == 'prev' && counter > 0) {
+		counter = counter - 1;
+		loadData();
+	} else if (type == 'next' && counter < customData.length) {
+		counter = counter + 1;
+		loadData();
+	}
+}
+
+function loadData() {
+  if (counter < customData.length && counter > -1) {
+ 	$('.display-key').html(customData[counter].key);
+  	$('.display-val').html(customData[counter].val);  
+  	$('.dim-areas').addClass('dim-areas');
+  } else {
+  	$('.dim-areas').removeClass('dim-areas');
+  }
+}
